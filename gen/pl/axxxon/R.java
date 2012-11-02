685c0197ef9f1186ee1b56b2257856175f59a257
Marcin S. Narowski <axxxon.pl@gmail.com>
2012-11-02 16:09:33 +0100

    app version beta 1.0

--- gen/pl/axxxon/R.java
+++ gen/pl/axxxon/R.java
@@ -10,19 +10,26 @@ package pl.axxxon;
 public final class R {
     public static final class attr {
     }
+
+    public static final class drawable {
+        public static final int ic_launcher = 0x7f020000;
+    }
+
     public static final class id {
-        public static final int clear=0x7f040004;
-        public static final int edit1=0x7f040001;
-        public static final int edit2=0x7f040002;
-        public static final int search=0x7f040005;
-        public static final int tablerow=0x7f040003;
-        public static final int widget0=0x7f040000;
+        public static final int clear = 0x7f050003;
+        public static final int edit1 = 0x7f050001;
+        public static final int edit2 = 0x7f050002;
+        public static final int main_layout = 0x7f050000;
+        public static final int results = 0x7f050005;
+        public static final int search = 0x7f050004;
     }
+
     public static final class layout {
-        public static final int extra=0x7f020000;
-        public static final int main=0x7f020001;
+        public static final int extra = 0x7f030000;
+        public static final int main = 0x7f030001;
     }
+
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name = 0x7f040000;
     }
 }
--- src/pl/axxxon/MainActivity.java
+++ src/pl/axxxon/MainActivity.java
@@ -1,26 +1,35 @@
 package pl.axxxon;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends Activity {
-    private final String APP_TAG = "RegexpTester";
+    private static final String APP_TAG = "RegexpTester";
+    private static final String PRE_MATCHES = "Found %d matches,\n";
+    private static final String MATCH_TEMPLATE = "%s - from %d to %d\n";
+
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.extra);
+        Log.d(APP_TAG, "Started");
 
         bindActions();
     }
 
-    private void bindActions(){
+    private void bindActions() {
         View clear = findViewById(R.id.clear);
         clear.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,19 +45,38 @@ public class MainActivity extends Activity {
                 runTest();
             }
         });
+        Log.d(APP_TAG, "Binded");
     }
 
-    public void clickClear(){
-
+    public void clickClear() {
+        EditText regexp = (EditText) findViewById(R.id.edit1);
+        EditText test = (EditText) findViewById(R.id.edit2);
+        EditText results = (EditText) findViewById(R.id.results);
+        regexp.setText("", TextView.BufferType.NORMAL);
+        test.setText("", TextView.BufferType.NORMAL);
+        results.setText("", TextView.BufferType.NORMAL);
+        Log.d(APP_TAG, "Cleared");
     }
 
-    public void runTest(){
+    public void runTest() {
+        Log.d(APP_TAG, "runned");
         EditText regexp = (EditText) findViewById(R.id.edit1);
         EditText test = (EditText) findViewById(R.id.edit2);
         String expression = regexp.getText().toString();
         String testString = test.getText().toString();
         Pattern pattern = Pattern.compile(expression);
-        boolean matches = pattern.matcher(testString).matches();
+        Matcher matcher = pattern.matcher(testString);
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        while (matcher.find()) {
+            sb.append(String.format(MATCH_TEMPLATE, matcher.group(), matcher.start(), matcher.end()));
+            count++;
+        }
+
+        String resultString = String.format(PRE_MATCHES, count);
+        resultString += sb.toString();
+        EditText results = (EditText) findViewById(R.id.results);
+        results.setText(resultString, TextView.BufferType.NORMAL);
 
     }
 }
