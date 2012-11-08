da8600f514df7a363dd29d9393d8868d0d305f19
Marcin S. Narowski <axxxon.pl@gmail.com>
2012-11-08 20:17:40 +0100

    new version

--- gen/pl/axxxon/regexptester/R.java
+++ gen/pl/axxxon/regexptester/R.java
@@ -10,26 +10,22 @@ package pl.axxxon.regexptester;
 public final class R {
     public static final class attr {
     }
-
     public static final class drawable {
-        public static final int ic_launcher = 0x7f020000;
+        public static final int ic_launcher=0x7f020000;
     }
-
     public static final class id {
-        public static final int clear = 0x7f050003;
-        public static final int edit1 = 0x7f050001;
-        public static final int edit2 = 0x7f050002;
-        public static final int main_layout = 0x7f050000;
-        public static final int results = 0x7f050005;
-        public static final int search = 0x7f050004;
+        public static final int clear=0x7f050003;
+        public static final int edit1=0x7f050001;
+        public static final int edit2=0x7f050002;
+        public static final int main_layout=0x7f050000;
+        public static final int results=0x7f050005;
+        public static final int search=0x7f050004;
     }
-
     public static final class layout {
-        public static final int extra = 0x7f030000;
-        public static final int main = 0x7f030001;
+        public static final int extra=0x7f030000;
+        public static final int main=0x7f030001;
     }
-
     public static final class string {
-        public static final int app_name = 0x7f040000;
+        public static final int app_name=0x7f040000;
     }
 }
